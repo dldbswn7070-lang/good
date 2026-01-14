53b67ad1aa55146fd61477dc43e8495f714a4e14
dldbswn7070-lang <dldbswn7070@gmail.com>
2026-01-15 01:52:12 +0900

    [SQL] Level 2 고양이와 개 / GROUP BY 활용

---
+++ SQL/Level2/3번_고양이와개.sql
@@ -0,0 +1,17 @@
+-- 문제: 고양이와 개는 몇 마리 있을까 (코드카타 SQL Level 2)
+-- 링크: https://school.programmers.co.kr/learn/courses/30/lessons/59040
+-- 날짜: 2026.01.14
+
+/*
+[문제 풀이 흐름 / 수도코드]
+1. 가져오기: ANIMAL_TYPE과 그 개수(COUNT)를 선택한다.
+2. 대상: ANIMAL_INS 테이블에서 가져온다.
+3. 그룹화: ANIMAL_TYPE(종류)별로 묶는다.
+4. 정렬: ANIMAL_TYPE을 기준으로 오름차순 정렬한다 (Cat -> Dog).
+*/
+
+-- [정답 코드]
+SELECT ANIMAL_TYPE, COUNT(*) AS count
+FROM ANIMAL_INS
+GROUP BY ANIMAL_TYPE
+ORDER BY ANIMAL_TYPE;
