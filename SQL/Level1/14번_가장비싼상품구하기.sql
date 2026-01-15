a1b1cd70e48eca8c2125f4ca178e5b279a07ddfa
dldbswn7070-lang <dldbswn7070@gmail.com>
2026-01-15 17:48:25 +0900

    [Update] 14번 문제 날짜 수정

--- SQL/Level1/14번_가장비싼상품구하기.sql
+++ SQL/Level1/14번_가장비싼상품구하기.sql
@@ -2,7 +2,7 @@
 -- 문제 출처: 부트캠프 SQL 코드카타 / 131697번
 -- 문제: 가장 비싼 상품 구하기 (프로그래머스 SQL Level 1)
 -- 링크: https://school.programmers.co.kr/learn/courses/30/lessons/131697
--- 날짜: 2026.01.16
+-- 날짜: 2026.01.04
 --문제풀이 블로그 링크:https://moon2moo.tistory.com/42
 
 /* [문제 포인트]
