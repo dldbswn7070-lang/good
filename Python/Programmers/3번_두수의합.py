184964836da1467bfba41114eee9492cc4501f00
dldbswn7070-lang <dldbswn7070@gmail.com>
2026-01-16 00:57:49 +0900

    [Python] Level 0 두 수의 합 / 함수 선언과 반환(return) 기초

---
+++ Python/Programmers/3번_두수의합.py
@@ -0,0 +1,21 @@
+# 3번_두수의합.py
+# 문제: 두 수의 합 (프로그래머스 Level 0) / 120802
+# 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120802
+# 날짜: 2026.01.15
+#문제풀이 블로그 링크 :https://moon2moo.tistory.com/67
+# 소요시간 : 1분
+
+def solution(num1, num2):
+   """
+    [코드 분석 & 문법 설명]
+    1. def solution(num1, num2):
+       - def: "나 지금 함수 만든다!" (선언)
+       - solution: "이 함수 이름은 solution이야."
+       - (num1, num2): "재료로 숫자 두 개를 받을 거야."
+       - : (콜론): "자, 이제 내용물 시작한다!"
+       
+    2. return num1 + num2
+       - num1 + num2: "받은 숫자 두 개를 더해서 계산해."
+       - return: "그리고 그 결과를 밖으로 던져줘(반환해)!"
+    """
+   return num1 + num2
