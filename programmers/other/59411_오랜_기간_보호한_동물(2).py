4188504f12fd85baf5d959b919ff85ee9c711f64
이윤주 <dldbswn7070@gmail.com>
2026-01-21 15:49:39 +0900

    [프로그래머스] 오랜 기간 보호한 동물(2) 풀이 제출
    - 문제 ID: 59411
    - 난이도: unknown
    - 제출일: 2026. 01. 21. 15:49:39
    - 작성자: 이윤주

---
+++ programmers/other/59411_오랜_기간_보호한_동물(2).py
@@ -0,0 +1,14 @@
+# 오랜 기간 보호한 동물(2)
+# 프로그래머스 (unknown)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/59411
+# 작성자: 이윤주
+# 작성일: 2026. 01. 21. 15:49:39
+
+-- 코드를 입력하세요
+SELECT O.ANIMAL_ID,
+       A.NAME
+FROM ANIMAL_OUTS O
+JOIN ANIMAL_INS A
+ON O.ANIMAL_ID = A.ANIMAL_ID
+ORDER BY O.DATETIME - A.DATETIME DESC
+LIMIT 2;
