141501fb02491e676a746a878db2a5408f87dc93
이윤주 <dldbswn7070@gmail.com>
2026-01-21 23:54:30 +0900

    [프로그래머스] L0 - 두 수의 나눗셈 (#4)

--- programmers/L0_기초입문/입문/004_두_수의_나눗셈.py
+++ programmers/L0_기초입문/입문/004_두_수의_나눗셈.py
@@ -3,7 +3,7 @@
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120806
 # 알고리즘: 기초
 # 작성자: 이윤주
-# 작성일: 2026. 01. 21. 23:48:50
+# 작성일: 2026. 01. 21. 23:54:29
 
 # 3분
 def solution(num1, num2):
@@ -11,7 +11,8 @@ def solution(num1, num2):
     return int(result)
 #다른 방식으로 푸는 방법:
 #def solution(num1, num2):
-#return (num1 * 1000) // num2 # num1 * 1000 하고 나누면 소수점 오차 줄일 수 있어요
-#// 사용하면 int() 없이도 정수로 나와요!
+#return (num1 * 1000) // num2 
+# num1 * 1000 하고 나누면 소수점 오차 줄일 수 있음
+#// 사용하면 int() 없이도 정수로 나온다고함!
 #def solution(num1, num2):
 #return int(num1 / num2 * 1000) 내가 푼 방식을 줄인 것
