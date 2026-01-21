120f809288ef0ada2e7570446c2fd513d0c977ea
이윤주 <dldbswn7070@gmail.com>
2026-01-21 23:39:41 +0900

    [프로그래머스] L0 - 나이 출력 (#3)

---
+++ programmers/L0_기초입문/입문/003_나이_출력.py
@@ -0,0 +1,16 @@
+# 나이 출력
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120820
+# 알고리즘: 기초
+# 작성자: 이윤주
+# 작성일: 2026. 01. 21. 23:39:41
+
+#5분
+
+def solution(age):
+    result = 2022 - age +1
+    return result
+
+#른 방식 : 
+#def solution(age):
+#return 2022 - age + 1
