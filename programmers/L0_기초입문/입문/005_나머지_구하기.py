ec05ce44cfaec0620fb831a35ac3d756a6875f74
이윤주 <dldbswn7070@gmail.com>
2026-01-21 23:52:54 +0900

    [프로그래머스] L0 - 나머지 구하기 (#5)

---
+++ programmers/L0_기초입문/입문/005_나머지_구하기.py
@@ -0,0 +1,14 @@
+# 나머지 구하기
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120810
+# 알고리즘: 기초
+# 작성자: 이윤주
+# 작성일: 2026. 01. 21. 23:52:53
+
+# 1분
+def solution(num1, num2):
+    result = num1 % num2
+    return result
+# 한 줄로 줄일 수 있음.
+#def solution(num1, num2):
+#return num1 % num2
